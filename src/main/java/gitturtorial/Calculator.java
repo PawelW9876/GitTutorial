c2e6f3e8abcde5a73c12ac8283cd3a4f3b0f5926
Paweł Wdowiak <pawel.wdowiak6789@gmail.com>
2018-01-23 19:52:29 +0100

    git commit -m "Repair added to the Calculator"

--- src/main/java/gitturtorial/Calculator.java
+++ src/main/java/gitturtorial/Calculator.java
@@ -1,6 +1,18 @@
 package gitturtorial;
 
+
 public class Calculator {
+    public float plus(float a, float b) {
+        return a + b;
+    }
+
+    public float minus(float a, float b) {
+        return a - b;
+    }
+
+    public float times(float a, float b) {
+        return a * b;
+    }
 
     public float by(float a, float b) {
         if (b == 0f) {
