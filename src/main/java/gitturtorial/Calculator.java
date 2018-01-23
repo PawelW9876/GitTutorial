7d9bd7511927ccb75cab5303345a923f8ad78830
Paweł Wdowiak <pawel.wdowiak6789@gmail.com>
2018-01-23 18:32:05 +0100

    git commit -m "Calculator"

---
+++ src/main/java/gitturtorial/Calculator.java
@@ -0,0 +1,17 @@
+package gitturtorial;
+
+public class Calculator {
+
+    public float plus(float a, float b) {
+        return a + b;
+    }
+    public float minus(float a, float b){
+        return a - b;
+    }
+    public float times(float a, float b){
+        return a*b;
+    }
+    public float by(float a, float b){
+        return a/b;
+    }
+}
