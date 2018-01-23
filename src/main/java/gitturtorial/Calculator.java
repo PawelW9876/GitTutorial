51d2b4668ec719433864ae1819b0b2f02f8194ee
Paweł Wdowiak <pawel.wdowiak6789@gmail.com>
2018-01-23 18:53:06 +0100

    git commit -m "Raise method added to the Calculator"

--- src/main/java/gitturtorial/Calculator.java
+++ src/main/java/gitturtorial/Calculator.java
@@ -2,16 +2,12 @@ package gitturtorial;
 
 public class Calculator {
 
-    public float plus(float a, float b) {
-        return a + b;
-    }
-    public float minus(float a, float b){
-        return a - b;
-    }
-    public float times(float a, float b){
-        return a*b;
-    }
-    public float by(float a, float b){
-        return a/b;
+    public int raise(int a, int b) {
+        int result = a;
+        for (int i = 1; i < b; i++){
+            result*=a;
+        }
+        return result;
+
     }
 }
